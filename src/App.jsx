652f012957855e8c8f25de8bd6d9f9624b6dbf38
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-07-01 09:11:38 +0700

    Setup the constants data for application

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Box } from "@mui/material"
 
+// Importing Without Default Initial
+/* 
 import Navbar from './components/Navbar'
 import Feed from "./components/Feed"
 import VideoDetail from "./components/VideoDetail"
 import ChannelDetail from "./components/ChannelDetail"
 import SearchFeed from "./components/SearchFeed"
+*/
+
+import {  
+  Navbar, 
+  Feed, 
+  VideoDetail, 
+  ChannelDetail,
+  SearchFeed } from "./components"
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Box sx={{ backgroundColor: '$000' }}>
+      <Box sx={{ backgroundColor: '#000' }}>
         <Navbar/>
         <Routes>
           <Route path="/" exact element={<Feed/>} />
---
+++ src/utils/constans.js
@@ -0,0 +1,34 @@
+import { MusicNote } from "@mui/icons-material";
+import { Home } from "@mui/icons-material";
+import { Code } from "@mui/icons-material";
+import { OndemandVideo } from "@mui/icons-material";
+import { SportsEsports } from "@mui/icons-material";
+import { LiveTv } from "@mui/icons-material";
+import { School } from "@mui/icons-material";
+import { FaceRetouchingNatural } from "@mui/icons-material";
+import { Checkroom } from "@mui/icons-material";
+import { GraphicEq } from "@mui/icons-material";
+import { TheaterComedy } from "@mui/icons-material";
+import { FitnessCenter } from "@mui/icons-material";
+import { DeveloperMode } from "@mui/icons-material";
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png'
+
+export const categories = [
+   {name: 'New', icon: <Home/>},
+   {name: 'Coding', icon: <Code/>},
+   {name: 'ReactJS', icon: <Code/>},
+   {name: 'NextJS', icon: <Code/>},
+   {name: 'Music', icon: <MusicNote/>},
+   {name: 'Education', icon: <School/>},
+   {name: 'Podcast', icon: <GraphicEq/>},
+   {name: 'Movie', icon: <OndemandVideo/>},
+   {name: 'Gaming', icon: <SportsEsports/>},
+   {name: 'Live', icon: <LiveTv/>},
+   {name: 'Sport', icon: <FitnessCenter/>},
+   {name: 'Fashion', icon: <Checkroom/>},
+   {name: 'Beauty', icon: <FaceRetouchingNatural/>},
+   {name: 'Comedy', icon: <TheaterComedy/>},
+   {name: 'Gym', icon: <FitnessCenter/>},
+   {name: 'Crypto', icon: <DeveloperMode/>},
+]
