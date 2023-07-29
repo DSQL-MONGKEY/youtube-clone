a336676dcd74ba25b602cc3e8dee622b98c41e0c
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-07-29 14:43:48 +0700

    initial create for Video detail

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,3 +1,11 @@
+import { useState, useEffect } from "react"
+import { Link, useParams } from "react-router-dom"
+import ReactPlayer from "react-player"
+import { Stack, Typography, Box } from "@mui/material"
+import { CheckCircle } from "@mui/icons-material"
+
+import Videos from "./Videos"
+import fetchAPI from "../utils/fetchAPI"
 
 const VideoDetail = () => {
   return (
