dde293e7b61a89bf1a930287a714a9578c6eeb90
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-07-30 22:14:27 +0700

    on debug (can't get id from params)

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -9,44 +9,44 @@ import fetchAPI from "../utils/fetchAPI"
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null)
-  const { id } = useParams()
+  const id = 'S4_O3R4E06A'
 
   useEffect(() => {
-    fetchAPI(`videos?id=7ghhRHRP6t4`)
+    fetchAPI(`videos?id=${id}`)
     .then(data => {
-      setVideoDetail(data.items)
+      setVideoDetail(data.items[0])
     })
   }, [id])
   console.log(videoDetail)
 
   // if(!videoDetail?.snippet) return <Typography sx={{ color: '#fff', fontSize: '20px' }}>LOADING</Typography>
 
-  // const { snippet: { title, channelId, channelTitle }, viewCount, likeCount } = videoDetail
+  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail
 
   return (
     <Box>
       <Stack direction={{ xs: 'column', md: 'row' }}>
         <Box flex={1}>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px'}}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=7ghhRHRP6t4`} 
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} 
             width='1/2'
             controls/>
-            <Typography>
-              {/* {title} */}
+            <Typography sx={{ fontSize: '20px', color: '#fff' }}>
+              {title}
             </Typography>
             <Stack direction='row' justifyContent='space-between' sx={{ color: '$fff' }} py='1px' px='2px'>
               <Link to={`/channel`}>
                 <Typography variant={{ sm: 'subtitle1', md: 'h6' }} sx={{ color: '#fff' }}>
-                  {/* {channelTitle} */}
+                  {channelTitle}
                   <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px' }} />
                 </Typography>
               </Link>
-              <Stack direction={`row`} gap={`20px`} alignItems={`center`}>
+              <Stack direction={`row`} gap={`20px`} alignItems={`center`} sx={{ color: '#fff' }}>
                 <Typography variant={`body1`} sx={{ opacity: 0.7 }}>
-                  {/* {parseInt(viewCount).toLocaleString()} | Views */}
+                  {parseInt(viewCount)} | Views
                 </Typography>
                 <Typography variant={`body1`} sx={{ opacity: 0.7 }}>
-                  {/* {parseInt(likeCount).toLocaleString()} | Likes */}
+                  {parseInt(likeCount)} | Likes
                 </Typography>
               </Stack>
             </Stack>
