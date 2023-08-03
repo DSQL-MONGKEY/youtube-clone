61c9c48e1be6437f5732805261af87edcd93b847
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-03 18:52:40 +0700

    Fix bugs

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -14,6 +14,7 @@ const Feed = () => {
     .then(data => setVideos(data.items))
   }, [selectedCategory])
 
+
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
       <Box sx={{ height: { sx:'auto', md:'92vh' }, borderRight: '1px solid #3d3d3d', px: { sm: 0, md: 2 } }}>
