f644df44966d46182aab111a9077c40f84948c22
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-08-07 22:12:51 +0700

    fix identation

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -34,35 +34,53 @@ const VideoDetail = () => {
       <Stack direction={{ xs: 'column', md: 'row' }} p={2}>
         <Box flex={1}>
           <Box 
-          sx={{ width: { xs: '100%'}, 
-          position: 'sticky', 
-          top: '86px',
-          }}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
-            width={'auto'} height={'500px'} controls/>
+            sx={{ width: { xs: '100%'}, 
+              position: 'sticky', 
+              top: '86px',
+            }}>
+            <ReactPlayer 
+              url={`https://www.youtube.com/watch?v=${id}`}
+              width={'auto'} 
+              height={'500px'} 
+              controls/>
             <Typography sx={{ fontSize: '20px', color: '#fff' }}>
               {title}
             </Typography>
-            <Stack direction='row' justifyContent='space-between' sx={{ color: '$fff' }} py='1px' px='2px'>
-              <Link to={`/channel`}>
-                <Typography variant={{ sm: 'subtitle1', md: 'h6' }} sx={{ color: '#fff' }}>
-                  {channelTitle}
-                  <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px' }} />
-                </Typography>
-              </Link>
-              <Stack direction={`row`} gap={`20px`} alignItems={`center`} sx={{ color: '#fff' }}>
-                <Typography variant={`body1`} sx={{ opacity: 0.7 }}>
-                  {parseInt(viewCount)} | Views
-                </Typography>
-                <Typography variant={`body1`} sx={{ opacity: 0.7 }}>
-                  {parseInt(likeCount)} | Likes
-                </Typography>
+            <Stack 
+              direction='row' 
+              justifyContent='space-between' 
+              sx={{ color: '$fff' }} 
+              py='1px' px='2px'>
+                <Link to={`/channelDetail/${id}`}>
+                  <Typography 
+                    variant={{ sm: 'subtitle1', md: 'h6' }} 
+                    sx={{ color: '#fff' }}>
+                      {channelTitle}
+                    <CheckCircle 
+                      sx={{ fontSize: '12px', color: 'gray', ml: '5px' }} />
+                  </Typography>
+                </Link>
+              <Stack 
+                direction={`row`} 
+                gap={`20px`} 
+                alignItems={`center`} 
+                sx={{ color: '#fff' }}>
+                  <Typography variant={`body1`} sx={{ opacity: 0.7 }}>
+                    {parseInt(viewCount)} | Views
+                  </Typography>
+                  <Typography variant={`body1`} sx={{ opacity: 0.7 }}>
+                    {parseInt(likeCount)} | Likes
+                  </Typography>
+                </Stack>
               </Stack>
-            </Stack>
+            </Box>
           </Box>
-        </Box>
-      <Box px={2} py={{ md: 1, xs: 5 }} justifyContent={`center`} alignItems={`center`}>
-          <Videos videos={videos} direction={'column'} />
+        <Box 
+          px={2} 
+          py={{ md: 1, xs: 5 }} 
+          justifyContent={`center`} 
+          alignItems={`center`}>
+            <Videos videos={videos} direction={'column'} />
         </Box>
       </Stack>
     </Box>
